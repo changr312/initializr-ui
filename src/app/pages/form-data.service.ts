400b057140f328d40562c1c6ac9b40d9f621d19e
Ryan Chang <ryan.chang@qvc.com>
2018-11-01 16:40:42 -0400

    dependency component

--- src/app/pages/form-data.service.ts
+++ src/app/pages/form-data.service.ts
@@ -3,15 +3,7 @@ import { Dependency } from "./models/dependency.model";
 
 @Injectable()
 export class FormDataService{
-    private dependency: Dependency = new Dependency();
 
-    setDependency(data: any){
-        this.dependency.name = data.dependency.name;
-        this.dependency.id = data.dependency.id;
-        this.dependency.description = data.dependency.description;
-        this.dependency.versionRange = data.dependency.versionRange;
-        this.dependency.groupId = data.dependency.groupId;
-        this.dependency.artifactId = data.dependency.artifactId;
-        this.dependency.version = data.dependency.version;
-    }
+
+    
 }

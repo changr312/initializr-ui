95cb5aef4afda810c25fd76351d947b714221ee5
Ryan Chang <ryan.chang@qvc.com>
2018-11-01 12:30:34 -0400

    misc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

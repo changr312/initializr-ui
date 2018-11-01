af612c03c768b3a8a2d6324525f725bfe742861d
Christopher Botlinger Jr <Christopher.BotlingerJr@qvc.com>
2018-11-01 14:01:47 -0400

    Added main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DependencyDashComponent } from './pages/dependency-dash/dependency-dash.component';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
+  { path: '', component: MainComponent},
   { path: 'dependency', component: DependencyDashComponent}
 ];
 @NgModule({

658b4e479d8c5593f1aa8bd462a20e7d656b9021
Ryan Chang <ryan.chang@qvc.com>
2018-11-01 20:33:06 -0400

    page template UI complete

--- src/app/pages/page-templates/page-templates.component.ts
+++ src/app/pages/page-templates/page-templates.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectTemplate } from '../models/project-template.model';
 import { FormDataService } from '../form-data.service';
-import { Dependency, DependencySection } from '../models/dependency.model';
+import { DependencySection } from '../models/dependency.model';
 
 @Component({
   selector: 'app-page-templates',
@@ -9,15 +9,17 @@ import { Dependency, DependencySection } from '../models/dependency.model';
   styleUrls: ['./page-templates.component.scss']
 })
 export class PageTemplatesComponent implements OnInit {
-  projectTemplateData: ProjectTemplate = {
-    projectTemplateNm: null,
-    dependencies: null,
-  };
+  projectTemplateData: ProjectTemplate = new ProjectTemplate();
 
   allDependencies: DependencySection = new DependencySection();
 
+  coreDependency: DependencySection = new DependencySection();
+  customDependency: DependencySection = new DependencySection();
+
   projectTemplateName;
   dependencies;
+
+  ids: string[] = [];
 
   constructor(
     private formDataService: FormDataService
@@ -27,14 +29,24 @@ export class PageTemplatesComponent implements OnInit {
     this.formDataService.getDependencies().subscribe(
       data =>{
         this.allDependencies = data;
+
+        this.coreDependency = this.allDependencies[0];
+        console.log(this.coreDependency);
+        this.customDependency = this.allDependencies[19];  
+        console.log(this.customDependency);      
       }
     );
+
+
   }
 
   onSubmit() {
-    console.log(this.projectTemplateName);
-    this.projectTemplateData.projectTemplateNm = this.projectTemplateName;
-    console.log(this.dependencies);
-    this.projectTemplateData.dependencies = this.dependencies;
+    this.projectTemplateData.dependencies = this.ids;
+    console.log(this.projectTemplateData);
+    }
+
+  checkboxChanged(id: string) {
+    this.ids.push(id);
   }
+
 }
